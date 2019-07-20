1063f7867ae91e0e5f8910d2a6688a177ea28afc
diana mujahed <diana.muj98@gmail.com>
2019-07-20 21:13:40 +0300

    run method is set

--- src/app/MainMenu.java
+++ src/app/MainMenu.java
@@ -7,5 +7,14 @@ public class MainMenu {
     private JButton UploadBtn;
     private JButton PracticeBtn;
     private JButton ExitBtn;
+public static void main(String [] args)
+{
+    JFrame MainMenu = new JFrame("MainMenu");
+    MainMenu.setContentPane(new MainMenu().panel1);
+    MainMenu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    MainMenu.pack();
+    MainMenu.setVisible(true);
+
+}
 
 }
