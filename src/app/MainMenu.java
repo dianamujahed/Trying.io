7fc58ec268403def791557a9b7564501ba00c57b
diana mujahed <diana.muj98@gmail.com>
2019-07-20 19:39:10 +0300

    solved jdk error

--- src/app/MainMenu.java
+++ src/app/MainMenu.java
@@ -1,4 +1,11 @@
 package app;
 
+import javax.swing.*;
+
 public class MainMenu {
+    private JPanel panel1;
+    private JButton UploadBtn;
+    private JButton PracticeBtn;
+    private JButton ExitBtn;
+
 }
