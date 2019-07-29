c075bdfcdad900c51d9b5c8c300e0449bc4091b4
diana mujahed <diana.muj98@gmail.com>
2019-07-29 23:23:44 +0300

    formate for practice

--- src/app/Practice.java
+++ src/app/Practice.java
@@ -1,56 +1,48 @@
 package app;
-import javax.swing.*;
+import javax.swing. * ;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.io.*;
 
 //this class is for making a thread for timer
- class Counter extends Thread{
-    public Counter(){
-
-    }
-    public void run(){
-		  /*
+class Counter extends Thread {
+    public Counter() {}
+    public void run() {
+		/*
 			1000 milliseconds = 1 second
 			60 seconds = 1 minute
 			60  * 1000  = 60,000 milliseconds
 			Reduce the thousands place, and you get 300 increments of 1000 milliseconds
 		  */
 
-		  Practice Time=new Practice();
-        for(int i = 59; i >= 0; i -= 1){
+            Practice Time = new Practice();
+            for (int i = 59; i >= 0; i -= 1) {
+                if (i == 0) {
+                    Result result = new Result();
+                    result.showResultFrame();
+                }
+                ;
 
-
-            if(i == 0) { Result result =new Result();
-                result.showResultFrame();
-            };
-
-            try{Thread.sleep(1000);
-                System.out.println(i);
-
-                Time.getTimeField().setText(" sec");//after each second until 60 sec, this code will execute
+                try {
+                    Thread.sleep(1000);
+                    System.out.println(i);
+                    Time.getTimeField().setText(" sec"); //after each second until 60 sec, this code will execute
+                } catch (InterruptedException e) {
+                    System.out.println(e.getStackTrace());
+                }
             }
-            catch(InterruptedException e){
-
-                System.out.println(e.getStackTrace());
-            }
-        }
 
     }
 }
 
 /*this class is for the practice GUI form */
-public class Practice   {
+public class Practice {
     private JTextArea OriginalCodeField;
     private JButton EndButton;
-    private JPanel mainPanel;
-    private JPanel practicePanel;
+    private JPanel MainPanel;
+    private JPanel PracticePanel;
     private JPanel TimePanel;
     private JPanel InputCodePanel;
     private JPanel EndPanel;
@@ -58,102 +50,59 @@ public class Practice   {
     private JLabel LanguageField;
     private JLabel LanguageLabel;
     private JLabel TimeLabel;
-    private  JLabel TimeField;
-    private JLabel practiceLabel;
+    private JLabel TimeField;
+    private JLabel PracticeLabel;
     private JTextArea UserInputArea;
     private String Language;
-    private static String OriginalCodeText="";
-    private  static JFrame Frame;
-    /**
-     * Open and read a file, and return the lines in the file as a list
-     * of Strings.
-     * (Demonstrates Java FileReader, BufferedReader, and Java5.)
-     */
-    private List<String> readFile(String filename)
-    {
-        List<String> records = new ArrayList<String>();
-        try
-        {
-            BufferedReader reader = new BufferedReader(new FileReader(filename));
-            String line;
-            while ((line = reader.readLine()) != null)
-            {
-                records.add(line);
-            }
-            reader.close();
-            return records;
-        }
-        catch (Exception e)
-        {
-            System.err.format("Exception occurred trying to read '%s'.", filename);
-            e.printStackTrace();
-            return null;
-        }
-    }
-
+    private static JFrame Frame;
+    private Counter counter;
 
     /*this method  shows the pracice form*/
-    public void showPracticeFrame()
-    {
-        Counter counter=new Counter();
+    public void showPracticeFrame() {
+        counter = new Counter();
         counter.start();
-         Frame= new JFrame("practice");
-        Frame.setContentPane(new Practice().mainPanel);
+        Frame = new JFrame("practice");
+        Frame.setContentPane(new Practice().MainPanel);
         Frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         Frame.setSize(500, 500);
         Frame.setVisible(true);
         Frame.setLocationRelativeTo(null);
-
     }
 
+    public Practice() {
+        OriginalCodeField.setLineWrap(true);
+        OriginalCodeField.setEditable(false);
+        OriginalCodeField.setCaretPosition(0);
+        UserInputArea.setLineWrap(true);
 
-
-public Practice()
-{
-    OriginalCodeField.setLineWrap(true);
-    OriginalCodeField.setEditable(false);
-    UserInputArea.setLineWrap(true);
-
-    java.awt.EventQueue.invokeLater(new Runnable() {
-        public void run() {
-            new Practice().TimeField.setVisible(true);
+        Upload UploadObject = new Upload();
+        Language = UploadObject.languageName();
+        if (Language == null) Language = "java";
+        LanguageField.setText(Language); //this code used to show the Language name in the practice form.
+        String FileName = Language + ".txt";
+        try {
+            FileReader fr = new FileReader(FileName);
+            BufferedReader br = new BufferedReader(fr);
+            String str;
+            while ((str = br.readLine()) != null)
+                OriginalCodeField.append(str + "\n"); //this code is for setting the original code of the language file  in the original code field
+            br.close();
         }
-    });
-    // when end button is clicked , the practice frame will be closed
-    EndButton.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
+        catch(IOException e) {
+            System.out.println("file not found");
+        }
 
+        // when end button is clicked , the practice frame will be closed
+        EndButton.addActionListener(new ActionListener() {@Override
+        public void actionPerformed(ActionEvent e) {
             Frame.dispose();
         }
-    });
-
-    Upload UploadObject=new Upload();
-    Language=UploadObject.languageName();
-    if(Language==null)
-        Language="java";
-    LanguageField.setText(Language);//this method used to show the Language name in the practice form.
-    String FileName= Language+".txt";
-
-    try {
-        FileReader fr=new FileReader(FileName);
-        BufferedReader br=new BufferedReader(fr);
-        String str;
-        while((str=br.readLine())!=null)
-        OriginalCodeField.append(str+"\n");//this code is for setting the original code of the language file  in the original code field
-
-    br.close();}
-    catch (IOException e){
-System.out.println("file not found");
-    }
+        });
     }
 
-//this method will return the TimeField variable
-public JLabel getTimeField(){
-    return TimeField;
-}
+    //this method will return the TimeField variable
+    public JLabel getTimeField() {
+        return TimeField;
+    }
 
 }
-
-
-
